6a175e6d103d48e0071e67f65cbca5c4fba97216
Kokinho00 <flaviocoutinho@hotmail.com>
2024-07-25 14:20:22 -0300

    secutity

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -81,6 +81,8 @@ public class Usuario {
 		return this.foto;
 	}
 
+	
+
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
@@ -92,5 +94,15 @@ public class Usuario {
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
 	}
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	
+	public Usuario() { }
 
 }
